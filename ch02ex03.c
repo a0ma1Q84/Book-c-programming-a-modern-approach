26475fb7f37d0a0ff1968b2eb9f5126ee8ffc8ef
a0ma1Q84 <a0ma1Q84@protonmail.com>
2023-10-14 10:32:22 +0200

    Replacing assignments with initializers.

--- ch02ex03.c
+++ ch02ex03.c
@@ -1,12 +1,31 @@
+/********************************************************************
+ * Filename: [ch02ex02.c]                                           *
+ * Author: [a0ma1Q84]                                               *
+ * Date: [October 13, 2023]                                         *
+ *                                                                  *
+ * Description:                                                     *
+ * This file is a solution to exercise 02, Section 2.2 from         *
+ * "C Programming: A Modern Approach, Second Edition" by K. N. King *
+ *                                                                  *
+ * Note:                                                            *
+ * This solution is solely developed for educational purposes and   *
+ * should not be distributed for commercial use.                    *
+ *******************************************************************/
+
+/*
+* Q: Condense the dweight.c program by : 
+* (1) replacing the assignments to height, length and width with initializers
+* (2) removing the weight variable, instead calculating (volume + 165) / 166 with the last printf.
+*/
+
+
 #include <stdio.h>
 
 int main(void) {
   
-  int height, length, width, volume, weight, random;
-
-  height = 8;
-  length = 12;
-  width = 10;
+  int height = 8, length = 12, width = 10;
+  int volume, weight;
+  
   volume = height * length * width;
   weight = (volume + 165) / 166;
   
