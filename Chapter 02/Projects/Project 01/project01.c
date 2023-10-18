3855decc346d64b877a809f83620b14c0bf30eb9
a0ma1Q84 <a0ma1Q84@protonmail.com>
2023-10-18 13:39:04 +0200

    Add project01.c

---
+++ Chapter 02/Projects/Project 01/project01.c
@@ -0,0 +1,27 @@
+/********************************************************************
+ * Filename: [project01.c]                                          *
+ * Author: [a0ma1Q84]                                               *
+ * Date: [October 18 2023]                                          *
+ *                                                                  *
+ * Description:                                                     *
+ * This file is a solution to project 01, Chapter 2 from            *
+ * "C Programming: A Modern Approach, Second Edition" by K. N. King *
+ *                                                                  *
+ * Note:                                                            *
+ * This solution is solely developed for educational purposes and   *
+ * should not be distributed for commercial use.                    *
+ *******************************************************************/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+    return 0;
+}
