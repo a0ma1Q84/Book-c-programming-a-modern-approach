e5f8d64785b2d7fde7a2d0493704a48a2543ff02
a0ma1Q84 <a0ma1Q84@protonmail.com>
2023-10-18 18:45:22 +0200

    add project 04

---
+++ Chapter 02/Projects/Project 04/project04.c
@@ -0,0 +1,33 @@
+/********************************************************************
+ * Filename: [project04.c]                                          *
+ * Author: [a0ma1Q84]                                               *
+ * Date: [October 18 2023]                                          *
+ *                                                                  *
+ * Description:                                                     *
+ * This file is a solution to project 04, Chapter 2 from            *
+ * "C Programming: A Modern Approach, Second Edition" by K. N. King *
+ *                                                                  *
+ * Note:                                                            *
+ * This solution is solely developed for educational purposes and   *
+ * should not be distributed for commercial use.                    *
+ *******************************************************************/
+
+#include <stdio.h>
+
+#define TAX_RATE 0.05f
+
+int main(void) 
+{   
+  float originalAmount, taxAmount, totalAmount;
+
+  /* Prompt the user to enter an amount */
+  printf("Enter an amount : ");
+  scanf("%f", &originalAmount);
+
+ /* Calculate the tax based on the user's input and add it to the original amount */
+  taxAmount = ( originalAmount * TAX_RATE );
+  totalAmount = ( originalAmount + taxAmount );
+
+  printf("With tax added : %.2f \n", totalAmount);
+  return 0;
+}
