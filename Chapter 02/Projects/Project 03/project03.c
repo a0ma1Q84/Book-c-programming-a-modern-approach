1d7c6c1aa80570b3458fb687da9ac6838fa2e65d
a0ma1Q84 <a0ma1Q84@protonmail.com>
2023-10-18 16:46:08 +0200

    Add project 03

---
+++ Chapter 02/Projects/Project 03/project03.c
@@ -0,0 +1,35 @@
+/********************************************************************
+ * Filename: [project03.c]                                          *
+ * Author: [a0ma1Q84]                                               *
+ * Date: [October 18 2023]                                          *
+ *                                                                  *
+ * Description:                                                     *
+ * This file is a solution to project 03, Chapter 2 from            *
+ * "C Programming: A Modern Approach, Second Edition" by K. N. King *
+ *                                                                  *
+ * Note:                                                            *
+ * This solution is solely developed for educational purposes and   *
+ * should not be distributed for commercial use.                    *
+ *******************************************************************/
+
+#include <stdio.h>
+
+#define PI 3.14159f
+#define SPHERE_VOLUME_FACTOR (4.0f / 3.0f)
+
+int main(void) 
+{   
+  /* Declare variables for the radius, volume, and the radius cubed */
+  float radius, volume, cubed_radius;
+
+  /* Prompt the user to enter the radius of the sphere */
+  printf("Enter radius of the sphere: ");
+  scanf("%f", &radius);
+ 
+  cubed_radius = radius * radius * radius;
+
+  volume = ( SPHERE_VOLUME_FACTOR * PI * cubed_radius);
+  printf("The volume of the sphere is %.2f cubic units\n", volume);
+
+  return 0;
+}
